50fb660c201721780ad12fe09811a6d435305785
Becky Ada <rebtshikadila@gmail.com>
2022-04-12 10:23:19 +0200

    création du modèle commentaire et sa migration

---
+++ database/migrations/2022_04_12_082052_create_commentaires_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentairesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('commentaires', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->text('commentaire');
+            $table->foreignId('article_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('commentaires');
+    }
+}
