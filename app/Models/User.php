ec546e3a478a0d065d7c9693fa6ea354b94a1504
Becky Ada <rebtshikadila@gmail.com>
2022-04-12 10:12:46 +0200

    création du model user et la migration

---
+++ app/Models/User.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'access',
+    ];
+
+    public function articles()
+    {
+        return $this->hasMany(Article::class);
+    }
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+}
