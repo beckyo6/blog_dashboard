1dd33be738287e5a0e16df57383754f28ed9049d
Becky Ada <rebtshikadila@gmail.com>
2022-04-13 10:39:50 +0200

    UI card stat des catégories sur la vue home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,5 +23,21 @@
                 </div>
             </div>
         </div>
+        <div class="col-md-4 ">
+            <div class="stats-small stats-small--1 card card-small">
+                <div class="card-body p-0 d-flex">
+                    <div class="d-flex flex-column m-auto">
+                        <div class="stats-small__data text-center">
+                            <span class="stats-small__label text-uppercase">Catégories</span>
+                            <h6 class="stats-small__value count my-3">0</h6>
+                        </div>
+                        <div class="stats-small__data">
+                            <span class="text-center">total</span>
+                        </div>
+                    </div>
+                    <canvas height="120" class="blog-overview-stats-small-1"></canvas>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
