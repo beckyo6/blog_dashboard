bd8777703f91043c299fcad4739d3567462b83a4
Becky Ada <rebtshikadila@gmail.com>
2022-04-13 10:33:56 +0200

    UI card stat des articles sur la vue home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,4 +6,22 @@
             <h3 class="page-title">Dashboard du blog</h3>
         </div>
     </div>
+    <div class="row">
+        <div class="col-md-4 ">
+            <div class="stats-small stats-small--1 card card-small">
+                <div class="card-body p-0 d-flex">
+                    <div class="d-flex flex-column m-auto">
+                        <div class="stats-small__data text-center">
+                            <span class="stats-small__label text-uppercase">Articles</span>
+                            <h6 class="stats-small__value count my-3">0</h6>
+                        </div>
+                        <div class="stats-small__data">
+                            <span class="text-center">total</span>
+                        </div>
+                    </div>
+                    <canvas height="120" class="blog-overview-stats-small-1"></canvas>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
