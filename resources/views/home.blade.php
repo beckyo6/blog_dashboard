813195d613757d5c8b705f52479d69994024b2db
Becky Ada <rebtshikadila@gmail.com>
2022-04-13 10:23:44 +0200

    titre sur la page d'accueil

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,9 @@
 @extends('templates.app')
+@section('content')
+    <div class="page-header row no-gutters py-4">
+        <div class="col-12 col-sm-4 text-center text-sm-left mb-0">
+            <span class="text-uppercase page-subtitle"></span>
+            <h3 class="page-title">Dashboard du blog</h3>
+        </div>
+    </div>
+@endsection
