1092f9223a1897ee3a30a431a08dca351f77e6c8
Marien Mupenda <marien.mupenda@gmail.com>
2022-05-01 21:07:54 +0200

    simplifier les actions  dans CategorieController

--- app/Http/Controllers/CategorieController.php
+++ app/Http/Controllers/CategorieController.php
@@ -3,14 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\Categorie;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class CategorieController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Application|Factory|View
      */
     public function index()
     {
@@ -22,7 +27,7 @@ class CategorieController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Application|Factory|View
      */
     public function create()
     {
@@ -32,10 +37,10 @@ class CategorieController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return Response
      */
-    public function store(Request $request)
+    public function store(Request $request): Response
     {
         $validated = $request->validate([
             'titre' => 'required',
@@ -53,8 +58,8 @@ class CategorieController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Categorie  $categorie
-     * @return \Illuminate\Http\Response
+     * @param Categorie $categorie
+     * @return Response
      */
     public function show(Categorie $categorie)
     {
@@ -64,22 +69,27 @@ class CategorieController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Categorie  $categorie
-     * @return \Illuminate\Http\Response
+     * @param Categorie $categorie
+     * @return Application|Factory|View|Response
      */
-    public function edit($id)
+    public function edit(Categorie $categorie)
     {
-        $categorie = Categorie::findOrFail($id);
-        return view('categories.update_categorie', compact('id', 'categorie'));
+        $data = [
+            'categorie' => $categorie,
+            'id' => $categorie->id,
+        ];
+
+        // je ne comprend pourquoi tu as besoin d'envoyer id alors que tu peux recuperér l'id dans le model
+        return view('categories.update_categorie', $data);
 
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Categorie  $categorie
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param Categorie $categorie
+     * @return Response
      */
     public function update(Request $request, Categorie $categorie)
     {
@@ -89,13 +99,14 @@ class CategorieController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Categorie  $categorie
-     * @return \Illuminate\Http\Response
+     * @param Categorie $categorie
+     * @return RedirectResponse
      */
-    public function destroy($categorie)
+    public function destroy(Categorie $categorie): RedirectResponse
     {
-        $categorie = Categorie::findOrFail($categorie);
-        $categorie->delete();
-        return redirect()->route('categories.index')->with('alert', 'la catégorie a été supprimée');
+       if( $categorie->delete()){
+           return redirect()->route('categories.index')->withSuccess('catégorie supprimer avec succès');
+       }
+       return redirect()->back()->withError('erreur à la suppression de la categorie');
     }
 }
